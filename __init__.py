cb71a594154a1e4abc86263272d3913636296f13
johndzxu <johndzxu@gmail.com>
2022-03-24 00:59:08 +0800

    initial commit

--- __init__.py
+++ __init__.py
@@ -1,17 +1,46 @@
-from mycroft import MycroftSkill, intent_file_handler
+import subprocess, os, re
+from mycroft import MycroftSkill, intent_handler
 
 
 class SystemControl(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.log.info("System Control Skill loaded")
 
-    @intent_file_handler('control.system.intent')
-    def handle_control_system(self, message):
-        application = message.data.get('application')
+    @intent_handler('ShutDown.intent')
+    def handle_shut_down_intent(self, message):
+        self.speak_dialog('shutdown')
 
-        self.speak_dialog('control.system', data={
-            'application': application
-        })
+    @intent_handler('OpenApp.intent')
+    def handle_open_app_intent(self, message):
+        app_name = message.data.get('app')
+        ls = subprocess.run(['ls ~/.local/share/applications/*.desktop'],
+                            shell=True,
+                            stdout=subprocess.PIPE,
+                            universal_newlines=True)
+        user_apps = ls.stdout.splitlines()
+        matches = [app for app in user_apps if app_name in app]
+        #print(user_apps)
+        #print(sorted(matches, key=len))
+        if matches:
+            with open(os.path.join('~/.local/share/applications/', sorted(matches, key=len)[0])) as f:
+                lines = f.readlines()
+                for line in lines:
+                    path = re.match(r'^Exec=(.*)', line)
+                    if path:
+                        self.log.info('Executing ' + path.group(1))
+                        launch = subprocess.run('exec ' + path.group(1),
+                                       shell=True,
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE,
+                                       universal_newlines=True)
+                        if not launch.stderr:
+                            self.speak_dialog('open.app', data={'app': app_name})
+                        else:
+                            self.speak(launch.stderr)
+
+        else:
+            self.speak('I did not find ' + app_name)
 
 
 def create_skill():
